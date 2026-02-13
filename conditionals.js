a9bdd540198903f70d012bef359645b8b2777af9
aitsalemassia-hue <aitsalemassia@gmail.com>
2026-02-13 22:42:26 +0100

    lab-conditionals-java

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+//console.log("Lab conditions JavaScript - démarrage OK");
+
+// programme 1 - are you old Enough?
+var age ='18';
+if (age >=65){
+     console.log("you get your income from your pension");
+}else if (age <65 && age >=18){
+     console.log("Each month you get a salary ");
+
+}else if (age <18){
+     console.log("you get an allowance ");
+
+}else{
+     console.log("the value of the age variable is not numerical ");
+}
---
+++ conditionals2.js
@@ -0,0 +1,27 @@
+// programme 2- days of week
+var day ="frday";
+switch(day){
+     case "Monday":
+          console.log("Read a book");
+          break;
+     case "Tuesday":
+          console.log("watch a movie");
+          break;
+     case "Wednesday":
+          console.log("go for a walk");
+          break;
+     case "Thursday":
+          console.log("play a game");
+          break;
+     case "Friday":
+          console.log("meet friends");
+          break;    
+     case "Saturday":
+          console.log("practise coding");
+          break;
+     case "Sunday":
+          console.log("rest and plan for the week ahead");
+          break;
+     default:
+          console.log("there is no such day");
+}
